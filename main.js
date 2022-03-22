edcb2822696cff90915345c3c4797223b4152487
Miguel Angel Contreras <miguel@andrepeat.xyz>
2022-03-22 01:47:44 -0600

    final

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const pokeId = document.getElementById("pokeId");
 const pokeTipo = document.getElementById("pokeTipo");
 const pokeEstadisticas = document.getElementById("pokeEstadisticas");
 //Diccionario de colores sacado desde https://gist.github.com/apaleslimghost/0d25ec801ca4fc43317bcff298af43c3
-const colours = {
+const coloresTipo = {
 	normal: '#A8A77A',
 	fire: '#EE8130',
 	water: '#6390F0',
@@ -25,10 +25,8 @@ const colours = {
 	dark: '#705746',
 	steel: '#B7B7CE',
 	fairy: '#D685AD',
+	defecto : '#FDF6EC'
 };
-
-
-
 
 // Funcion para buscar pokemon desde <form action="" onsubmit="buscarPokemon(event)">
 const buscarPokemon = event => {
@@ -37,16 +35,86 @@ const buscarPokemon = event => {
     fetch(`https://pokeapi.co/api/v2/pokemon/${value.toLowerCase()}`)
         .then(data => data.json())
         .then(respuesta => mostrarPokemon(respuesta))
+		.catch(err => mostrarNoEncontrado(value))
+		return value
 }
 const mostrarPokemon = data =>{
     const sprite = data.sprites.other.home.front_default;
     const { stats, types} = data;
     console.log(data)
     // Mostrar el nombre en <div id="pokeName" class="pokeNameStyle">Pokedex</div>
-    pokeName.textContent = data.name;
+    pokeName.textContent = data.name.toUpperCase();
+	pokeName.style.fontWeight = `500`;
+	pokeName.style.fontStyle= 'italic';
+	pokeName.style.fontSize = `2rem`;
+	pokeName.style.color = coloresTipo[types[0].type.name];
+	tarjetaPokemon.style.background = `white`;
     // Mostrar la imagen en <img id="pokeImagen" src="./assets/cual.webp" alt="¿Quién es este pokemon?">
     pokeImagen.setAttribute('src',sprite);
     //Mostrar el numero del pokemon en <div id="pokeId"></div>
     pokeId.textContent= `ID ${data.id}`
+	pokeId.style.fontSize = `1.5rem`;
+	pokeId.style.color = coloresTipo[types[0].type.name];
+	colorearContenedor(types);
+	mostrarTypePokemon(types);
+	mostrarPokemonEstadisticas(stats);
+}
 
+const colorearContenedor = types => {
+	const colorUno = coloresTipo[types[0].type.name];
+	const colorDos = types[1] ? coloresTipo[types[1].type.name]: coloresTipo.defecto;
+	pokeImagen.style.background= `white`;
+	pokeImagenContainer.style.background= `linear-gradient(  ${colorUno},${colorDos} )`;
+	// pokeImagen.style.borderStyle = `solid`;
+	// pokeImagen.style.borderWidth = `.5rem`;
+	// pokeImagen.style.borderImage= `linear-gradient(${colorUno},${colorDos} )30`;
+	pokeImagen.style.backgroundSize = `100%`;
+	tarjetaPokemon.style.borderStyle = `solid`;
+	tarjetaPokemon.style.borderWidth = `.5rem`;
+	tarjetaPokemon.style.borderImage = `linear-gradient(${colorUno},${colorDos})30`;
+}
+
+const mostrarTypePokemon = types => {
+	pokeTipo.innerHTML = '';
+	types.forEach(type => {
+		const elementoTextoType = document.createElement("div");
+		elementoTextoType.style.backgroundColor = coloresTipo[type.type.name];
+		elementoTextoType.textContent = type.type.name.toUpperCase();
+		elementoTextoType.style.color = `white`;
+
+		pokeTipo.appendChild(elementoTextoType);		
+	});
+}
+
+const mostrarPokemonEstadisticas = stats => {
+	pokeEstadisticas.innerHTML ='';
+	stats.forEach(stat => {
+		const elementoEstadisticas = document.createElement("div");
+		const elementoEstadisticasNombre = document.createElement("div");
+		const elementoEstadisticasValor = document.createElement("div");
+		elementoEstadisticasNombre.textContent = stat.stat.name.toUpperCase();
+		elementoEstadisticasValor.textContent = stat.base_stat;
+		elementoEstadisticasValor.style.fontWeight = `500`;
+		elementoEstadisticasValor.style.fontSize = `1.3rem`;
+		elementoEstadisticas.appendChild(elementoEstadisticasNombre);
+		elementoEstadisticas.appendChild(elementoEstadisticasValor);
+		pokeEstadisticas.appendChild(elementoEstadisticas);
+	
+	});
+}
+
+const mostrarNoEncontrado = value => {
+	pokeName.textContent = `${value} no Encontrado`;
+	pokeName.style.fontWeight = `500`;
+	pokeName.style.fontSize = `2rem`;
+	pokeName.style.color = `white`;
+	pokeImagen.setAttribute('src','./assets/egg.png');
+	pokeImagenContainer.style.background = `white`;
+	tarjetaPokemon.style.background = `red`;
+	tarjetaPokemon.style.borderWidth = `0`;
+	pokeEstadisticas.innerHTML='';
+	pokeTipo.innerHTML='';
+	pokeId.innerHTML='';
+
+	
 }
